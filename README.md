826a6d57dd1626f0c98f1f6e21a812b18d661ca9
Void <david.lu@connect.qut.edu.au>
2023-11-29 11:00:52 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# TwitterLinkFixer
- Discord bot to fix twitter links so they embed
+# LinkFixer
+A Discord bot to replace twitter.com and x.com links in discord with fxtwitter.com and fixupx.com so they embed in messages.
+
+The bot will delete the original message and replace the link with the correct link to embed. If text is in the message it will also keep it in the new message.
+
+Invite the bot [here.](https://discord.com/oauth2/authorize?client_id=1179216622339248138&scope=bot&permissions=8192)
---
+++ main.py
@@ -0,0 +1,80 @@
+import os
+import discord
+import re
+import asyncio
+from discord.ext import commands
+from discord.ext import tasks
+from dotenv import load_dotenv
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+# Function to load the replacement count from a file
+def load_replacement_count():
+    try:
+        with open('replacement_count.txt', 'r') as file:
+            return int(file.read())
+    except FileNotFoundError:
+        return 0
+
+# Function to save the replacement count to a file
+def save_replacement_count(count):
+    with open('replacement_count.txt', 'w') as file:
+        file.write(str(count))
+
+replacement_count = load_replacement_count()  # Load the initial count
+
+@tasks.loop(minutes=5)
+async def change_presence_task():
+    global replacement_count
+    await bot.change_presence(activity=discord.Game(name="Replacing twitter links"))
+    await asyncio.sleep(30)  # Wait for 30 seconds
+    await bot.change_presence(activity=discord.Game(name=f"{replacement_count} links replaced"))
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+    change_presence_task.start()  # Start the presence-changing task)
+
+@bot.event
+async def on_message(message):
+    global replacement_count  # Declare the variable as global
+
+    if message.author == bot.user:
+        return  # Skip processing messages sent by the bot itself
+
+    # Define the URLs to check for and the replacement text
+    urls_to_replace = {
+        'x.com': 'https://fixupx.com',
+        'twitter.com': 'https://fxtwitter.com'
+    }
+
+    for url, replacement in urls_to_replace.items():
+        # Use regular expressions to find URLs with the specified domains
+        pattern = re.compile(f'https?://(www\.)?{re.escape(url)}', re.IGNORECASE)
+        replaced_content = pattern.sub(replacement, message.content)
+
+        if replaced_content != message.content:
+            #print(f'Replaced "{url}" with "{replacement}" in message: {message.content}')
+            
+            # Increment the replacement count
+            replacement_count += 1
+            save_replacement_count(replacement_count) # Save the updated count to the file
+
+            # Delete the original message
+            await message.delete()
+
+            # Send a new message with the replaced content
+            await message.channel.send(f"{message.author}:\n{replaced_content}")
+
+    await bot.process_commands(message)
+
+# Command to check the replacement count
+@bot.command()
+async def linkcount(ctx):
+    global replacement_count
+    await ctx.send(f"The bot has replaced {replacement_count} links.")
+
+load_dotenv()
+bot.run(os.getenv('TOKEN'))
